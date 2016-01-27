145a3f908e3d2756d46872799a4dba77c02ae27a
Kirk Walker Graves <graves.kirk@gmail.com>
2016-01-27 16:04:41 -0600

    completed problem 21 and corrected 19 and 20

--- 19.sql
+++ 19.sql
@@ -1,4 +1,5 @@
 /* Which sales agent made the most in sales in 2009? HINT: MAX */
 
-SELECT q.'Employee Name', MAX(q.'Total Sales') AS 'Total' FROM (SELECT Employee.FirstName || ' ' || Employee.LastName AS 'Employee Name', ROUND(SUM(Invoice.Total),2) AS 'Total Sales'
-FROM Invoice JOIN Customer ON Customer.CustomerId == Invoice.CustomerId JOIN Employee ON Employee.EmployeeId == Customer.SupportRepId GROUP BY Customer.SupportRepId) AS q;
+SELECT EmployeeName AS 'Employee Name', MAX(SalesIn2009) AS '2009 Sales'
+FROM (SELECT Employee.FirstName || ' ' || Employee.LastName AS EmployeeName, ROUND(SUM(Invoice.Total), 2) AS SalesIn2009
+FROM Invoice JOIN Customer ON Customer.CustomerId == Invoice.CustomerId JOIN Employee ON Employee.EmployeeId == Customer.SupportRepId WHERE strftime('%Y', Invoice.InvoiceDate) == "2009");
--- 20.sql
+++ 20.sql
@@ -1,5 +1,5 @@
 /* Which sales agent made the most in sales in 2010? */
 
 SELECT EmployeeName AS 'Employee Name', MAX(SalesIn2010) AS '2010 Sales'
-FROM (SELECT Employee.FirstName || ' ' || Employee.LastName AS EmployeeName, SUM(Invoice.Total) AS SalesIn2010
+FROM (SELECT Employee.FirstName || ' ' || Employee.LastName AS EmployeeName, ROUND(SUM(Invoice.Total), 2) AS SalesIn2010
 FROM Invoice JOIN Customer ON Customer.CustomerId == Invoice.CustomerId JOIN Employee ON Employee.EmployeeId == Customer.SupportRepId WHERE strftime('%Y', Invoice.InvoiceDate) == "2010");
